5781b6824f660fd828a6739220b972cdf24f82ae
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-12-03 22:06:36 +0100

    test route api

---
+++ restaurant_laravel/app/Http/Controllers/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+
+    //liste contacts
+    public function index()
+    {
+        $contacts=Contact::all();
+
+        return response()->json(['contacts'=>$contacts]);
+        # code...
+    }
+
+    // creation contact
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'firstname' => 'required|string',
+            'lastname' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string'
+        ]);
+       $contact=Contact::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'message' => $request->message,
+        ]);
+        return response()->json([
+            'message' => 'Contact created.',
+            'contact' => $contact
+        ], 201);
+    }
+}
--- restaurant_laravel/routes/api.php
+++ restaurant_laravel/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Models\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
