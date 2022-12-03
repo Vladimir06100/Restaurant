17207319e71f60da6451244fc10b54621114aa5c
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-12-04 00:24:24 +0100

    test api manque affichage

--- restaurant_react/src/App.js
+++ restaurant_react/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+//import { ReactDOM } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+      Contacts( );
+  }, []);
+  
+  async function Contacts() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch('http://localhost:8000/api/contacts', options);
+    const data = await response.json();
+    console.log(data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Youpi
-        </a>
-      </header>
+      <div>
+        <h1>Liste des Contacts</h1>
+        <ul>
+          <li>
+            <p>Prénom :  </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
-}
+  }
+
 
 export default App;
