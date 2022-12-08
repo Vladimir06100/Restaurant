bb64cce180a9bbd0a76258a881b93582a95df5bc
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-12-08 10:54:38 +0100

    Migration plus db seed restaurateurs

---
+++ restaurant_laravel/database/factories/RestaurateurFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Restaurateur>
+ */
+class RestaurateurFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'nom' => fake()->name(),
+            'prenom' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => fake()->password(),
+            //Hash a la fin
+            //'password' => Hash::make(Str::random(10)), // password pas de droits de stockage en clair
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
