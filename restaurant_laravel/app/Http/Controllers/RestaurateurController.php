5222a58dd6422eca79a10a24a20b2e60b4533054
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-12-08 11:26:37 +0100

    control Restaurateur plus routes

--- restaurant_laravel/app/Http/Controllers/RestaurateurController.php
+++ restaurant_laravel/app/Http/Controllers/RestaurateurController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurateur;
 use Illuminate\Http\Request;
 
 class RestaurateurController extends Controller
 {
     //affiche profile
+    public function show($id)
+    {
+        $restaurateur = Restaurateur::find($id);
+        return response()->json(['restaurateur' => $restaurateur]);
+    }
+
+    // creation profile
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required|string',
+            'prenom' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+        $restaurateur = Restaurateur::create([
+            'nom' => $request->nom,
+            'prenom' => $request->prenom,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+        return response()->json([
+            'message' => 'Restaurateur created.',
+            'restaurateur' => $restaurateur
+        ], 201);
+    }
     
 }
--- restaurant_laravel/routes/api.php
+++ restaurant_laravel/routes/api.php
@@ -19,3 +19,7 @@ use App\Models\Contact;
 Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
 
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+
+// routes pour les restaurateurs (inscription, connexion, profil)
+Route::post('/restaurateurs', [RestaurateurController::class, 'store'])->name('restaurateurs.store');
+Route::get('/restaurateurs/{id}', [RestaurateurController::class, 'show'])->name('restaurateurs.show');
