d466410dae562b91b6de6b39cb5d80008c8cbeb4
Bastien <bastfv@gmail.com>
2022-12-09 15:40:44 +0100

    Nouvelle branche

--- restaurant_react/src/App.js
+++ restaurant_react/src/App.js
@@ -1,90 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import './App.css';
 import Contact from './Contact/Contact';
+import Error404 from "./Components/Error404/error404";
+import LoginPage from "./Pages/login_page";
+
+const router = createBrowserRouter ([
+
+  {
+    path: "/",
+    element: <Contact />,
+    error: <Error404 />
+  },
+
+  {
+    path: "/login",
+    element: <LoginPage />,
+    error: <Error404 />
+  },
+]);
 
 function App() {
-  const [contacts, setContacts] = useState([]);
 
-  async function getContacts() {
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    let response = await fetch('http://localhost:8000/api/contacts', options);
-    const data = await response.json();
-    console.log(data);
-    const contacts = data.contacts;
-    setContacts(contacts);
-  }
-
-  useEffect(() => {
-    console.log('useEffect');
-    getContacts();
-  }, []);
-
-  async function createContact(firstname, lastname, email, message) {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        message: message,
-      }),
-    };
-    let response = await fetch('http://localhost:8000/api/contacts', options);
-
-    if (response.status !== 201) {
-      return;
-    }
-
-    const data = await response.json();
-
-    const newContact = data.contact;
-    setContacts([newContact, ...contacts]);
-  }
 
 
   return (
     <div className="App">
-      <div>
-        <h1>Liste des Contacts</h1>
-        {/* create form for new contact */}
-        <form onSubmit={(event) => {
-          event.preventDefault();
-          const firstname = event.target.firstname.value;
-          const lastname = event.target.lastname.value;
-          const email = event.target.email.value;
-          const message = event.target.message.value;
-          createContact(firstname, lastname, email, message);
-        }}>
-          <label htmlFor="firstname">Prénom</label>
-          <input type="text" name="firstname" id="firstname" />
-          <label htmlFor="lastname">Nom</label>
-          <input type="text" name="lastname" id="lastname" />
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="30" rows="10"></textarea>
-          <button type="submit">Envoyer</button>
-        </form>
-        <ul>
-          {contacts.map((contact, index) => (
-            <Contact
-              key={index}
-              firstname={contact.firstname}
-              lastname={contact.lastname}
-              email={contact.email}
-              message={contact.message}
-            />
-          ))}
-        </ul>
-      </div>
+
+      <RouterProvider router={router} />
+      
     </div>
   );
 }
