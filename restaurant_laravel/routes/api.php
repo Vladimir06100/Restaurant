7cb9f052aaa4600d03348ba922788eba91a64716
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-12-11 17:25:34 +0100

    mise a jour routes api (nettoyage)

--- restaurant_laravel/routes/api.php
+++ restaurant_laravel/routes/api.php
@@ -17,24 +17,15 @@ use App\Http\Controllers\RestaurantController;
 |
 */
 
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-
 // routes pour les restaurateurs (inscription, profil)
 Route::post('/restaurateurs', [RestaurateurController::class, 'store'])->name('restaurateurs.store');
 Route::get('/restaurateurs/{id}', [RestaurateurController::class, 'show'])->name('restaurateurs.show');
 
-// routes pour la connexion
+// routes pour la connexion des restaurateurs
 Route::post('/restaurateurs/login', [RestaurateurController::class, 'login'])->name('restaurateurs.login');
 
-
-// resource pour les restaurants (creation, affichage)
+// resource pour les restaurants les 7 routes
 Route::resource('restaurants', RestaurantController::class);
 
-// routes pour les produits (creation, affichage)
+// routes pour les produits les 7 routes
 Route::resource('produits', ProduitController::class);
-
-
-
-Route::post('/produits', [ProduitController::class, 'store'])->name('produits.store');
-Route::get('/produits', [ProduitController::class, 'index'])->name('produits.index');
