88540d22a38f125c6fe022b737b6169d7d3b1298
Thom <thomayor@gmail.com>
2022-12-12 15:59:09 +0100

    carte props

--- restaurant_react/src/Pages/Cartes.js
+++ restaurant_react/src/Pages/Cartes.js
@@ -2,31 +2,33 @@ import Menu from '../Components/Menu';
 import Footer from '../Components/Footer';
 // import ''./Cartes.css';
 import {useState,useEffect} from 'react';
+import Cartes_props from '../Props/Cartes_props';
+
 
 function Cartes() {
 
- const [cartes, setCartes] = useState ([]);
+    const [cartes, setCartes] = useState ([]);
 
-   async function getCartes() {
-     const options = {
-       method: 'GET',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-     };
-     let response = await fetch('http://localhost:8000/api/cartes', options);
-     const data = await response.json();
-     console.log(data);
-     const cartes = data.cartes;
-     setCartes(cartes);
-   }
-
-   useEffect(() => {
-     console.log('useEffect');
-     getCartes();
-   }, []);
-
-   async function createCarte(nom_carte) {
+    async function getCartes() {
+      const options = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      let response = await fetch('http://localhost:8000/api/cartes', options);
+      const data = await response.json();
+      console.log(data);
+      const cartes = data.cartes;
+      setCartes(cartes);
+    }
+ 
+    useEffect(() => {
+      console.log('useEffect');
+      getCartes();
+    }, []);
+ 
+    async function createCarte(nom_carte) {
      const options = {
        method: 'POST',
        headers: {
@@ -38,50 +40,54 @@ function Cartes() {
        }),
      };
      let response = await fetch('http://localhost:8000/api/cartes', options);
-
+ 
      if (response.status !== 201) {
        return;
      }
-
+ 
      const data = await response.json();
-
+ 
      const newCarte = data.carte;
      setCartes([newCarte, ...cartes]);
    }
-
-
-
     return (
         <div>
             <Menu />
             <h1>Cartes</h1>
+    
 
             <div>
-                <form method="POST" action="" onSubmit={(event)=> {
-const nom_carte= event.target.nom_carte.value;
-createCarte(nom_carte);
+            <form method="POST" action="" onSubmit={(event)=> {
+        const nom_carte= event.target.nom_carte.value;
+        createCarte(nom_carte);
 
 
-                }}>
-                    <label htmlFor="name">Nom de la carte</label>
-                    <input type="text" id="name" name="nom_carte" />
+        }}>
+             {cartes.map((carte, index) => (
+  <Cartes_props 
+  key={index}
+  nom_carte={carte.nom_carte} 
+  produit_id={carte.produit_id} />
+  ))}
 
-                    <label htmlFor="produit">Produits</label>
-                    <select id="produit" name="produit">
-                        <option value="produit_id">   {cartes.map((carte, index) => (
-             <Cartes
-               key={index}
-               produit_id={carte.produit_id}
-             />
-           ))}</option>
-                    </select>
 
-                    <button type="submit">Ajouter</button>
-                    <button type="submit">Modifier</button>
-                    <button type="submit">Supprimé</button>
-                </form>
+            <label htmlFor="name">Nom de la carte</label>
+            <input type="text" id="name" name={cartes.nom_carte}  />
 
-                
+            <label htmlFor="produit">Produits</label>
+            <select id="produit" name="produit">
+                <option value={cartes.produit_id}>  
+    
+   </option>
+            </select>
+
+            <button type="submit">Ajouter</button>
+            <button type="submit">Modifier</button>
+            <button type="submit">Supprimé</button>
+        </form>
+
+        
+
             </div>
             <Footer />
         </div>
---
+++ restaurant_react/src/Props/Cartes_props.js
@@ -0,0 +1,22 @@
+import{useState,useEffect} from 'react';
+
+function Cartes({
+	postnom_carte,postproduit_id
+}) {
+
+	
+	return (
+
+        <div>
+
+Nom Carte : {postnom_carte}
+
+Produits : {postproduit_id}
+
+      
+    </div>
+				
+	
+	);
+}
+export default Cartes;
