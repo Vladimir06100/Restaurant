7cceb3b312166c9f9eb8419d38bb7f2bec1a357c
Fleurot <fleurot.damien190@gmail.com>
2022-12-13 09:56:24 +0100

    qrcode test

---
+++ restaurant_react/src/Pages/Qrcode/Qrcode.js
@@ -0,0 +1,45 @@
+import "restaurant_react/src/pages/Qrcode/Qrcode.css"
+import QRCode from 'qrcode'
+import { useState } from 'react'
+
+function Qrcode() {
+
+	let fakeLink = []
+
+	const [url, setUrl] = useState('')
+	const [qr, setQr] = useState('')
+
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, {
+			width: 800,
+			margin: 2,
+			color: {
+				dark: '#6FDFBE',
+				light: '#EEEEEEFF'
+			}
+		}, (err, url) => {
+			if (err) return console.error(err)
+
+			console.log(url)
+			setQr(url)
+		})
+	}
+ 
+	return (
+		<div className="app">
+			<h1>Générateur de QR Code</h1>
+			<input 
+				type="text"
+				placeholder="Quel site? "
+				value={url}
+				onChange={e => setUrl(e.target.value)} />
+			<button onClick={GenerateQRCode}>Générer</button>
+			{qr && <>
+				<img src={qr} alt='link qr'/>
+				<a href={qr} download="qrcode.png">Télécharger le QRCode</a>
+			</>}
+		</div>
+	)
+}
+
+export default Qrcode;
