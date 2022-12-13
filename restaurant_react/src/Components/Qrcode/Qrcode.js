3e76647003ebebb1b8b2d401804473c6656690e0
Fleurot <fleurot.damien190@gmail.com>
2022-12-13 14:15:06 +0100

    QrCode OK

--- restaurant_react/src/Components/Qrcode/Qrcode.js
+++ restaurant_react/src/Components/Qrcode/Qrcode.js
@@ -1,50 +1,14 @@
-import QRCode from 'qrcode';
-import { useState } from 'react';
-import '../../Styles/Qrcode.css';
+import QRCode from 'qrcode.react'; //Import du modude Qrcode nécessaire pour générer un Qrcode. 
+import '../../Styles/Qrcode.css'; 
 
 function Qrcode() {
-
-	let fakelink = [
-		"https://facebook.com",
-		"https://twitter.com",
-		"https://www.linkedin.com",
-		"https://plus.google.com",
-		"https://plus.google.com",
-	]
-
-	const [url, setUrl] = useState('')
-	const [qr, setQr] = useState('')
-
-	const GenerateQRCode = () => {
-		QRCode.toDataURL(url, {
-			width: 800,
-			margin: 2,
-			color: {
-				dark: '#6FDFBE',
-				light: '#EEEEEEFF'
-			}
-		}, (err, url) => {
-			if (err) return console.error(err)
-
-			console.log(url)
-			setQr(url)
-		})
-	}
  
 	return (
-		<div className="qrcode_div">
-			<h1>Générateur de QR Code</h1>
-			<input 
-				type="text"
-				placeholder="Quel site? "
-				value={url}
-				onChange={e => setUrl(e.target.value)} />
-			<button onClick={GenerateQRCode}>Générer</button>
-			{qr && <>
-				<img src={qr} alt='link qr'/>
-				<a href={qr} download="qrcode.png">Télécharger le QRCode</a>
-			</>}
-		</div>
+			<div className="qrcode_header">
+
+			<QRCode  // La balise QRCode permet de créer un qrcode.
+               value="https://mettrelelienici.com"style={{ marginRight: 50 }}/>
+			</div>
 	)
 }
 
