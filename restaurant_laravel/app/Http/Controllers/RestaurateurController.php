0d9f753d7c6ad98fcb7743365f18517aa486cafe
Bastien <bastfv@gmail.com>
2022-12-14 10:07:51 +0100

    hash

--- restaurant_laravel/app/Http/Controllers/RestaurateurController.php
+++ restaurant_laravel/app/Http/Controllers/RestaurateurController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Restaurateur;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 
 // RegisterController 
 class RestaurateurController extends Controller
