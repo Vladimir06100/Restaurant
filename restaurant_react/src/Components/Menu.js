c0a66d39f7adb0a4b40be0c28c2ef9f2cd924a6a
Cassandra <cassandra.sw@live.fr>
2022-12-15 09:33:58 +0100

    modif majeur du style + menu + formulaire + ajout icon

--- restaurant_react/src/Components/Menu.js
+++ restaurant_react/src/Components/Menu.js
@@ -1,4 +1,4 @@
-import logo from '../Images/logo.png';
+// import logo from '../Images/logo.png';
 import '../Styles/Menu.css';
 import { useState, useEffect } from 'react';
 
@@ -13,9 +13,9 @@ function Menu() {
     return (
         <div>
             <section className="MainMenu">
-                <div className="logoMenu">
+                {/* <div className="logoMenu">
                     <img src={logo} alt='logo' />
-                </div>
+                </div> */}
 
                 <nav className="navbar">
                     <ul className="links desktop-size">
@@ -48,6 +48,16 @@ function Menu() {
                                     <li className="link"><a href="/restaurant">AJOUTER UN RESTAURANT </a></li>
                                     <li className="link"><a href="/produits">PRODUITS </a></li>
                                     <li className="link"><a href="/cartes">CARTES </a></li>
+                                    <li>
+                                        <span id="navbar_style">
+                                            <a href="/connexion">CONNEXION </a>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <span id="navbar_style">
+                                            <a href="/inscription">INSCRIPTION </a>
+                                        </span>
+                                    </li>
                                 </ul>
                             </div>
                         </ul> :
@@ -62,72 +72,73 @@ function Menu() {
         </div>
 
 
-        // <div>
-        //     <div className="navigation_espace">
-        //         <div className="navbar_logo">
-        //             <img src={logo} alt='logo' />
-        //         </div>
-
-        //         <nav className="navbar">
-        //             <div className="navLinks">
-        //                 <ul>
-        //                     <li><a href="/">HOME </a></li>
-        //                     <li><a href="/restaurant">AJOUTER UN RESTAURANT </a></li>
-        //                     <li><a href="/produits">PRODUIT </a></li>
-        //                     <li><a href="/cartes">Cartes </a></li>
-        //                     <li>
-        //                         <span id="navbar_style">
-        //                             <a href="/connexion">CONNEXION </a>
-        //                         </span>
-        //                     </li>
-        //                     <li>
-        //                         <span id="navbar_style">
-        //                             <a href="/inscription">INSCRIPTION </a>
-        //                         </span>
-        //                     </li>
-        //                 </ul>
-        //             </div>
-        //         </nav>
-
-        //         {/* rend le menu en burger pour les portables */}
-        //         <div className="navMenu">
-        //             {isOpen ?
-        //                 <a id="closeButton" onClick={() => setIsOpen(false)}>
-        //                     <span className="material-symbols-outlined">
-        //                         close
-        //                     </span>
-
-        //                     <div className="navLinksBurger">
-        //                         <ul>
-        //                             <li><a href="/">HOME </a></li>
-        //                             <li><a href="/restaurant">AJOUTER UN RESTAURANT </a></li>
-        //                             <li><a href="/produits">PRODUIT </a></li>
-        //                             <li><a href="/cartes">Cartes </a></li>
-        //                             <li>
-        //                                 <span id="navbar_style">
-        //                                     <a href="/connexion">CONNEXION </a>
-        //                                 </span>
-        //                             </li>
-        //                             <li>
-        //                                 <span id="navbar_style">
-        //                                     <a href="/inscription">INSCRIPTION </a>
-        //                                 </span>
-        //                             </li>
-        //                         </ul>
-        //                     </div>
-        //                 </a> :
-
-        //                 <a id="openButton" onClick={() => setIsOpen(true)}>
-        //                     <span className="material-symbols-outlined">
-        //                         menu
-        //                     </span>
-
-        //                 </a>
-        //             }
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
 export default Menu;
+
+// <div>
+//     <div className="navigation_espace">
+//         <div className="navbar_logo">
+//             <img src={logo} alt='logo' />
+//         </div>
+
+//         <nav className="navbar">
+//             <div className="navLinks">
+//                 <ul>
+//                     <li><a href="/">HOME </a></li>
+//                     <li><a href="/restaurant">AJOUTER UN RESTAURANT </a></li>
+//                     <li><a href="/produits">PRODUIT </a></li>
+//                     <li><a href="/cartes">Cartes </a></li>
+//                     <li>
+//                         <span id="navbar_style">
+//                             <a href="/connexion">CONNEXION </a>
+//                         </span>
+//                     </li>
+//                     <li>
+//                         <span id="navbar_style">
+//                             <a href="/inscription">INSCRIPTION </a>
+//                         </span>
+//                     </li>
+//                 </ul>
+//             </div>
+//         </nav>
+
+//         {/* rend le menu en burger pour les portables */}
+//         <div className="navMenu">
+//             {isOpen ?
+//                 <a id="closeButton" onClick={() => setIsOpen(false)}>
+//                     <span className="material-symbols-outlined">
+//                         close
+//                     </span>
+
+//                     <div className="navLinksBurger">
+//                         <ul>
+//                             <li><a href="/">HOME </a></li>
+//                             <li><a href="/restaurant">AJOUTER UN RESTAURANT </a></li>
+//                             <li><a href="/produits">PRODUIT </a></li>
+//                             <li><a href="/cartes">Cartes </a></li>
+//                             <li>
+//                                 <span id="navbar_style">
+//                                     <a href="/connexion">CONNEXION </a>
+//                                 </span>
+//                             </li>
+//                             <li>
+//                                 <span id="navbar_style">
+//                                     <a href="/inscription">INSCRIPTION </a>
+//                                 </span>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </a> :
+
+//                 <a id="openButton" onClick={() => setIsOpen(true)}>
+//                     <span className="material-symbols-outlined">
+//                         menu
+//                     </span>
+
+//                 </a>
+//             }
+//         </div>
+//     </div>
+// </div>
--- restaurant_react/src/Pages/Restaurant.js
+++ restaurant_react/src/Pages/Restaurant.js
@@ -1,5 +1,9 @@
 import Menu from '../Components/Menu';
 import Footer from '../Components/Footer';
+import avatar from '../Images/Icones/avatar.gif';
+import location from '../Images/Icones/location.gif';
+import phone from '../Images/Icones/phone.gif';
+import time from '../Images/Icones/time.gif';
 import '../Styles/Restaurant.css';
 
 function Restaurant() {
@@ -7,56 +11,66 @@ function Restaurant() {
         <div>
             <Menu />
             <div className="restaurant_position">
+                <div className="positionBis">
 
-                <div className="home_title">
-                    <span>
-                        Add your <br /><span id="home_title_color">restaurant</span>
-                    </span>
+                    <div className="home_title">
+                        <span>
+                            Add your <br /><span id="home_title_color">restaurant</span>
+                        </span>
 
-                    <div className="restaurant_text">
-                        <p>
-                            Lorem ipsum dolor sit amet. Qui rerum voluptatem eum blanditiis ratione qui sunt nulla eum adipisci corporis a rerum voluptas et doloremque nisi qui velit eligendi? Aut voluptatibus consequatur non laboriosam maxime ut ducimus dicta. Est quam asperiores aut ducimus veniam nam numquam necessitatibus ut consequatur quaerat qui fuga optio aut nihil laboriosam.
-                        </p>
+                        <div className="restaurant_text">
+                            <p>
+                                Lorem ipsum dolor sit amet. Qui rerum voluptatem eum blanditiis ratione qui sunt nulla eum adipisci corporis a rerum voluptas et doloremque nisi qui velit eligendi? Aut voluptatibus consequatur non laboriosam maxime ut ducimus dicta. Est quam asperiores aut ducimus veniam nam numquam necessitatibus ut consequatur quaerat qui fuga optio aut nihil laboriosam.
+                            </p>
+                        </div>
+
                     </div>
+                    <div className="formRestaurant">
+                        <form method="POST" action="">
 
-                </div>
-                <div className="formRestaurant">
-                    <form method="POST" action="">
+                            <div className="textbox">
+                                <label htmlFor="name"></label>
+                                <input type="text" id="name" name="name" placeholder="Your name.." required />
+                                <span><img src={avatar} alt="avatar" /></span>
+                                {/* icone static ou en gif.*/}
+                                {/* <span class="material-symbols-outlined">
+                                    badge
+                                </span> */}
+                            </div>
 
-                        <div className="textbox">
-                            <label htmlFor="name"></label>
-                            <input type="text" id="name" name="name" placeholder="Your name.." required />
-                            <span class="material-symbols-outlined">
-                                badge
-                            </span>
-                        </div>
+                            <div className="textbox">
+                                <label htmlFor="adresse"></label>
+                                <input type="text" id="adresse" name="adresse" placeholder="Your adresse.." required />
+                                <span><img src={location} alt="location" /></span>
+                                {/* icone static ou en gif.*/}
+                                {/* <span class="material-symbols-outlined">
+                                    home
+                                </span> */}
+                            </div>
 
-                        <div className="textbox">
-                            <label htmlFor="adresse"></label>
-                            <input type="text" id="adresse" name="adresse" placeholder="Your adresse.." required />
-                            <span class="material-symbols-outlined">
-                                home
-                            </span>
-                        </div>
+                            <div className="textbox">
+                                <label htmlFor="phone"></label>
+                                <input type="text" id="phone" name="phone" placeholder="Your phone.." required />
+                                <span><img src={phone} alt="phone" /></span>
+                                {/* icone static ou en gif.*/}
+                                {/* <span class="material-symbols-outlined">
+                                    call
+                                </span> */}
+                            </div>
 
-                        <div className="textbox">
-                            <label htmlFor="phone"></label>
-                            <input type="text" id="phone" name="phone" placeholder="Your phone.." required />
-                            <span class="material-symbols-outlined">
-                                call
-                            </span>
-                        </div>
+                            <div className="textbox">
+                                <label htmlFor="horaires"></label>
+                                <input type="text" id="horaires" name="horaires" placeholder="Your horaires.." required />
+                                <span><img src={time} alt="time" /></span>
+                                {/* icone static ou en gif.*/}
+                                {/* <span class="material-symbols-outlined">
+                                    schedule
+                                </span> */}
+                            </div>
 
-                        <div className="textbox">
-                            <label htmlFor="horaires"></label>
-                            <input type="text" id="horaires" name="horaires" placeholder="Your horaires.." required />
-                            <span class="material-symbols-outlined">
-                                schedule
-                            </span>
-                        </div>
-
-                        <input id="submitCo" type="submit" name="en" onclick="alert('Vôtre restaurant à été crée')" />
-                    </form>
+                            <input id="submitCo" type="submit" name="en" onclick="alert('Vôtre restaurant à été crée')" />
+                        </form>
+                    </div>
                 </div>
             </div>
             <Footer />
