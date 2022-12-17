3dfcd044046f7d2ac9ad3cabacc959a256f08335
Bastien <bastfv@gmail.com>
2022-12-17 12:05:31 +0100

    Modif login + install sanctum

--- restaurant_laravel/app/Models/Restaurateur.php
+++ restaurant_laravel/app/Models/Restaurateur.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -19,7 +18,8 @@ class Restaurateur extends Authenticatable
 
     protected $hidden = [
         'password',
-        'token',
+        'remember_token',
+        //'token',
     ];
 
     protected $fillable = [
@@ -27,7 +27,6 @@ class Restaurateur extends Authenticatable
         'prenom',
         'email',
         'password',
-        'token',
         'role'
     ];
 
