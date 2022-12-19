d404aa2f3acaae4bc530b521cc703500146d3477
Fleurot <fleurot.damien190@gmail.com>
2022-12-19 11:22:43 +0100

    Réparation base de donnée

--- restaurant_react/src/Components/Menu.js
+++ restaurant_react/src/Components/Menu.js
@@ -20,8 +20,8 @@ function Menu() {
                 <nav className="navbar">
                     <ul className="links desktop-size">
                         <li className="link"><a href="/">HOME </a></li>
-                        <li className="link"><a href="/creationrestaurant">AJOUTER UN RESTAURANT </a></li>
-                        <li className="link"><a href="/mesrestaurants">MES RESTAURANTS </a></li>
+                        <li className="link"><a href="/creation/restaurant">AJOUTER UN RESTAURANT </a></li>
+                        <li className="link"><a href="/restaurants">MES RESTAURANTS </a></li>
                         <li className="link"><a href="/produits">PRODUIT </a></li>
                         <li className="link"><a href="/cartes">CARTES </a></li>
                         <li>
