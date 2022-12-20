7b8195211639a995262eaf6b0fbaa16168f502ed
ibmow <kibrand@hotmail.fr>
2022-12-20 05:04:26 +0100

    rectif function formules_props

--- restaurant_react/src/Props/Formules_props.js
+++ restaurant_react/src/Props/Formules_props.js
@@ -5,29 +5,26 @@ function Formules(props) {
         <table>
             <thead>
                 <tr>
-                    <th>Nom_formule</th>
+                    <th>nom_formule</th>
                     <th>description_formule</th>
-                    <th>entree</th>
-                    <th>plat</th>
-                    <th>dessert</th>
+                    <th>categorie_produit_entree</th>
+                    <th>categorie_produit_plat</th>
+                    <th>categorie_produit_dessert</th>
                     <th>prix_formule</th>
                     <th>votre_prix</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>{props.Nom_formule}</td>
-                    <td>{props.type}</td>
-                    {/* <td>{props.categories.type}</td> */}
-                    {/* <td>{categorie_id}</td> */}
-                    <td>{props.entree}</td>
-                    <td>{props.plat}</td>
-                    <td>{props.dessert}</td>
+                    <td>{props.nom_formule}</td>
+                    <td>{props.description_formule}</td>
+                    <td>{props.categorie_produit_entree}</td>
+                    <td>{props.categorie_produit_plat}</td>
+                    <td>{props.categorie_produit_dessert}</td>
                     <td>{props.prix_formule}</td>
                     <td>{props.votre_prix}</td>
                     <td>
-                    <button className="btn">Modifier</button>
-                    <button className="btn">Supprimer</button>
+                    
                  </td>
                 </tr>
             </tbody>
