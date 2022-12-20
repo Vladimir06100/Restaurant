d6f532fd5db35e52661e9fec88213388181531e8
Rouslan abaev <abaev.rouslan@hotmail.fr>
2022-12-20 09:36:42 +0100

    correctif page cartes

--- restaurant_react/src/Pages/Cartes.js
+++ restaurant_react/src/Pages/Cartes.js
@@ -7,57 +7,56 @@ import Cartes_props from "../Props/Cartes_props";
 
 function Cartes() {
 
-    const [cartes, setCartes] = useState ([]);
-    const [listproduits, setListproduits] = useState ([]);
+  const [cartes, setCartes] = useState ([]);
+  const [listproduits, setListproduits] = useState ([]);
 
-    async function getCartes() {
-      const options = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      let response = await fetch('http://localhost:8000/api/cartes', options);
-      const data = await response.json();
-      console.log(data);
-      const cartes = data.cartes;
-      const listproduits= data.liste_des_produits;
-      setCartes(cartes);
-      setListproduits(listproduits);
-      console.log(listproduits,'affichage listes');
+  async function getCartes() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    let response = await fetch('http://localhost:8000/api/cartes', options);
+    const data = await response.json();
+    console.log(data);
+    const cartes = data.cartes;
+    const listproduits= data.liste_des_produits;
+    setCartes(cartes);
+    setListproduits(listproduits);
+    console.log(listproduits,'affichage listes');
+  }
+
+  useEffect(() => {
+    getCartes();
+  }, []);
+
+
+  async function createCarte(nom_carte, produit_id) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        nom_carte:nom_carte,
+        produit_id:produit_id
+        // jajoute le produit produit_id:produit  
+      }),
+    };
+      
+    let response = await fetch('http://localhost:8000/api/cartes', options);
+  
+    if (response.status !== 201) {
+      return;
     }
- 
-    useEffect(() => {
-      getCartes();
-    }, []);
- 
-
-    async function createCarte(nom_carte, produit_id) {
-        const options = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            nom_carte:nom_carte,
-            produit_id:produit_id
-            // jajoute le produit produit_id:produit
-           
-          }),
-        };
-        let response = await fetch('http://localhost:8000/api/cartes', options);
-    
-        if (response.status !== 201) {
-          return;
-        }
-    
-        const data = await response.json();
-    
-        const newCarte = data.carte;
-        setCartes([newCarte, ...cartes]);
-      }
+  
+    const data = await response.json();
+  
+    const newCarte = data.carte;
+    setCartes([newCarte, ...cartes]);
+  }
    
-
   return (
     <div>
       <Menu />
@@ -66,54 +65,53 @@ function Cartes() {
       <div className="cartesPosition">
         <div className="cartesPositionBis">
 
-            <form method="POST" action="" onSubmit={(event)=> {
-                event.preventDefault();
-                const nom_carte= event.target.nom_carte.value;
-                const produit_id= event.target.produit_id.value
-                //j'ajoute le prduit
-                createCarte(nom_carte,produit_id);
-                
+          <form method="POST" action="" onSubmit={(event)=> {
+            event.preventDefault();
+            const nom_carte= event.target.nom_carte.value;
+            const produit_id= event.target.produit_id.value
+            //j'ajoute le prduit
+            createCarte(nom_carte,produit_id);
+            
             console.log(nom_carte, "nom" );
-            }}>
-            <label htmlFor="nom_carte">Nom de la carte</label>
-            <input type="text" id="nom_carte" name="nom_carte" />
+          }}>
+
+          <label htmlFor="nom_carte">Nom de la carte</label>
+          <input type="text" id="nom_carte" name="nom_carte" />
      
 
-
-            <label htmlFor="produit">Produits</label>
-            <select id="produit" name="produit_id">
+          <label htmlFor="produit">Produits</label>
+          <select id="produit" name="produit_id">
   
-         
-            {listproduits.map(produit => 
-					  <option key={produit.id} value={produit.id}> {produit.nom_produit} -
+          {listproduits.map(produit => 
+					<option key={produit.id} value={produit.id}> {produit.nom_produit} -
                      - {produit.categorie} -
                       {produit.description}</option>
-				
-		
-    )};
-            </select>
-
+          )};
+            
+          </select>
             <button type="submit">Ajouter</button>
-        </form>
+          </form>
 
 
-            <div>
+          <div>
        
+            <h2> Affichage </h2>
 
-<h2> Affichage </h2>
-        {cartes.map((carte, index) => (
-  <Cartes_props 
-  key={index}
-  nom_carte={carte.nom_carte} 
-  listproduits={listproduits}
-  produit_id={carte.produit_id}  
-  />
-  ))}
+              {cartes.map((carte, index) => (
+              <Cartes_props 
+              key={index}
+              nom_carte={carte.nom_carte} 
+              listproduits={listproduits}
+              produit_id={carte.produit_id}  
+              />))}
 
-            </div>
-            <Footer />
+          </div>
+
+          <Footer />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Cartes;
