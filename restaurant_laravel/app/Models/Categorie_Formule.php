5df7fc0af62effe0538a1653e651f2b48eb1ad7b
ibmow <kibrand@hotmail.fr>
2022-12-20 07:00:22 +0100

    rectif

--- restaurant_laravel/app/Models/Categorie_Formule.php
+++ restaurant_laravel/app/Models/Categorie_Formule.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Categorie_Formule extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'formule1' =>['entree', 'plat', 'dessert'],
+        'formule2' => ['plat', 'dessert'],
+        'formule3' => ['entree', 'plat']
+    ];
+
+    private string $formule1;
+    private string $formule2;
+    private string $formule3;
+
 }
