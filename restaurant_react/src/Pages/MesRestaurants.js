38911d34a3531b4bd0f1837294f415693fb0f3b0
Thom <thomayor@gmail.com>
2022-12-21 11:31:59 +0100

    ok

--- restaurant_react/src/Pages/MesRestaurants.js
+++ restaurant_react/src/Pages/MesRestaurants.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Footer from '../Components/Footer';
 import Menu from '../Components/Menu';
-import "../Styles/MesRestaurants.css";
 import{ Link } from 'react-router-dom';
+import "../Styles/MesRestaurants.css";
 
 function MesRestaurants() {
 
