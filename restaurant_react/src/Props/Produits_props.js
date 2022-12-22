17ce184153102c01e0a8eee4e5646761d1aa9464
Cassandra <cassandra.sw@live.fr>
2022-12-22 09:36:48 +0100

    commencement de la page product list

--- restaurant_react/src/Props/Produits_props.js
+++ restaurant_react/src/Props/Produits_props.js
@@ -1,35 +1,38 @@
+import '../Styles/Produits.css';
+
 function Produit(props) {
-    // coucou
-    //bouh
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Catégorie</th>
-                    <th>Description</th>
-                    <th>PrixHT</th>
-                    <th>TVA</th>
-                    <th>PrixTTC</th>
-                    <th>Quantité</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{props.nom_produit}</td>
-                    <td>{props.categorie_id}</td>
-                    <td>{props.description}</td>
-                    <td>{props.prixHT}</td>
-                    <td>{props.TVA}</td>
-                    <td>{props.prixTTC}</td>
-                    <td>{props.quantite}</td>
-                    <td>
-                        <button onClick={props.update} className="btn">Modifier</button>
-                        <button onClick={() => props.destroy(props.id)} className="btn">Supprimer</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Catégorie</th>
+                        <th>Description</th>
+                        <th>PrixHT</th>
+                        <th>TVA</th>
+                        <th>PrixTTC</th>
+                        <th>Quantité</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{props.nom_produit}</td>
+                        <td>{props.categorie_id}</td>
+                        <td>{props.description}</td>
+                        <td>{props.prixHT}</td>
+                        <td>{props.TVA}</td>
+                        <td>{props.prixTTC}</td>
+                        <td>{props.quantite}</td>
+                        <td>
+                            <button onClick={props.update} className="btn-modif-product">Modifier</button>
+                            <button onClick={() => props.destroy(props.id)} className="btn-supp-product">Supprimer</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
     );
 }
 
